3750a67dbf8c4199bc446ed907095654d4d35afe
DanielBorbafs <dev.danielborba@gmail.com>
2024-08-01 13:18:10 -0300

    Feat: add sidebar function

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/* .card-nav{
+    position: absolute;
+    top: 0;
+    right: 0;
+    background: black;
+    width: 626px;
+    height: 950px;
+}
+
+.chama-navbar {
+    font-size: 34px;
+}
+*/
+
+const btnNav = document.querySelector('.chama-navbar')
+const sidebar = document.querySelector('.card-nav')
+
+
+btnNav.addEventListener('click', () => {
+    if(sidebar.style.display === 'none' || sidebar.style.display === '') {
+        sidebar.style.display = 'block';
+        sidebar.classList.remove('leave');
+        sidebar.classList.add('enter');
+        sidebar.classList.add('visible');
+    } else {
+        sidebar.style.display = 'none';
+        sidebar.classList.remove('enter');
+        sidebar.classList.add('leave');
+        sidebar.classList.remove('visible');
+    }
+});
+
